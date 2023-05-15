a866a85688333373504c4ea429df3c2839e635fb
wimdavies <william.johnston.davies@gmail.com>
2023-05-15 14:32:29 +0100

    #all spaces repository method, GET /spaces route

--- app.rb
+++ app.rb
@@ -1,12 +1,26 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/database_connection'
+require_relative 'lib/space_repository'
+
+if ENV['ENV'] != 'test'
+  DatabaseConnection.connect('rubnb')
+end
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/space_repository'
   end
 
   get '/' do
     return erb(:index)
+  end
+
+  get '/spaces' do
+    repo = SpaceRepository.new
+    @spaces = repo.all
+
+    return erb(:spaces)
   end
 end
